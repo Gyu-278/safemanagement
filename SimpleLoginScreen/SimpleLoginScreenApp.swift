8a9d2a697ad5721b40e88a602b60350040ec7f0c
KrystalZhang612 <csezhang@ucdavis.edu>
2022-06-16 21:46:36 -0700

    Initial Commit

---
+++ SimpleLoginScreen/SimpleLoginScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleLoginScreenApp.swift
+//  SimpleLoginScreen
+//
+//  Created by erika on 6/16/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleLoginScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
